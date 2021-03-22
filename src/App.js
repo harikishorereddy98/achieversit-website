95c70aefc0effd24fd701fd15b570497f74ad82e
Hari <harikishorereddy96@gmail.com>
2021-03-22 14:30:24 -0700

    reacjs-router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+// import Counter from './components/Counter';
+import Nav from './components/Nav';
 
+// import Corousel from './components/Corousel';
+import Header from './components/Header';
+// import Become from './components/Become';
+// import Trendingcourses from './components/Trendingcourses';
+// import Recentadditions from './components/Recentadditions';
+// import Discover from './components/Discover';
+// import Review from './components/Review';
+// import Enroll from './components/Enroll';
+// import Acheivers from './components/Acheivers';
+// import Whyweare from './components/whyweare';
+// import Ourtrainers from './components/Ourtrainers';
+// import Latestjobs from './components/Latestjobs';
+// import Ourclients from './components/Ourclients';
+import Footer from './components/Footer';
+import Contact from './components/Contact';
+// import Uidevelopment from './components/Uidevelopment';
+// import {Switch,Route, Router} from 'react-router-dom'
+import Router from './components/Router';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+     
+     
+     <Header/>
+    <Nav/>
+     <Router/> 
+<Footer/>
+<Contact/>
     </div>
   );
 }
